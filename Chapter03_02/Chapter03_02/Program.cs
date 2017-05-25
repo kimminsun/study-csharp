543778fd64fc83af3693d6a831c883d4ff7d4ddf
kimminsun <minsun2767@naver.com>
2017-05-25 11:07:54 +0900

    #5 csharp class study

--- Chapter03_02/Chapter03_02/Program.cs
+++ Chapter03_02/Chapter03_02/Program.cs
@@ -31,6 +31,73 @@ namespace Chapter03_02
                 Console.WriteLine( "{0}:{1}", iter, tmpP[ iter ] );
             }
 
+            List<int> tmpList = new List<int>();
+
+            tmpList.Add( 35 );
+            tmpList.Add( 27 );
+            tmpList.Add( 10 );
+
+            Console.WriteLine( tmpList[ 0 ] );
+            Console.WriteLine( tmpList[ 1 ] );
+
+            foreach(int iter in tmpList)
+            {
+                Console.WriteLine( iter );
+            }
+            tmpList.Sort();
+            foreach(int iter in tmpList)
+            {
+                Console.WriteLine( iter );
+            }
+
+            List<string> strList = new List<string>();
+            strList.Add( "VIPS" );
+            strList.Add( "Vshley" );
+            strList.Add( "Outback" );
+
+            strList.Sort();
+            foreach(string iter in strList)
+            {
+                Console.WriteLine( iter );
+            }
+            if(strList.Contains("VIPS")==true)
+            {
+                Console.WriteLine( "Find!" );
+            }
+
+            List<List<string>> itemList = new List<List<string>>();
+            List<string> itemAttack = new List<string>();
+            itemAttack.Add( "Sword" );
+            itemAttack.Add( "Axe" );
+            itemAttack.Add( "Spear" );
+            itemList.Add( itemAttack );
+
+            List<string> itemDefend = new List<string>();
+            itemDefend.Add( "Shield" );
+            itemDefend.Add( "Armor" );
+            itemList.Add( itemDefend );
+
+            List<string> itemPortion = new List<string>();
+            itemPortion.Add( "Healing" );
+            itemPortion.Add( "Flying" );
+            itemPortion.Add( "Fog" );
+            itemPortion.Add( "Fast" );
+            itemList.Add( itemPortion );
+
+            foreach(List<string>iterList in itemList)
+            {
+                foreach(string iterStr in iterList)
+                {
+                    Console.Write( iterStr );
+                    Console.Write( " " );
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine( itemList[ 0 ][ 1 ] );
+            Console.WriteLine( itemList[ 1 ][ 0 ] );
+            Console.WriteLine( itemList[ 2 ][ 2 ] );
+
             Console.ReadKey();
         }
     }
