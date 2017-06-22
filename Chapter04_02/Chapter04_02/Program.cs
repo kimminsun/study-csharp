aeb194351414159fe8bfac1d105f44d55735560a
kimminsun <minsun2767@naver.com>
2017-06-22 11:33:57 +0900

    #13 chsarp class study

--- Chapter04_02/Chapter04_02/Program.cs
+++ Chapter04_02/Chapter04_02/Program.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chapter04_02
+namespace Chapter04_01
 {
     class Program
     {
         public delegate int calculate( int a, int b );
 
-        public static int Plus(int a,int b)
+        public static int Plus( int a, int b )
         {
             Console.WriteLine( "{0}+{1}={2}", a, b, a + b );
             return (a + b);
         }
-        public static int Minus(int a,int b)
+        public static int Minus( int a, int b )
         {
             Console.WriteLine( "{0}-{1}={2}", a, b, a - b );
             return (a - b);
@@ -30,10 +30,9 @@ namespace Chapter04_02
             Console.WriteLine( "{0}/{1}={2}", a, b, a - b );
             return (a / b);
         }
-
         public calculate DelegateOper;
         public event calculate EventOper;
-        
+
         static void Main( string[] args )
         {
             calculate cal1;
@@ -43,7 +42,7 @@ namespace Chapter04_02
 
             calculate cal2;
             cal2 = new calculate( Multiply );
-            int res2 = cal2( 4,5 );
+            int res2 = cal2( 4, 5 );
             Console.WriteLine( "return:{0}", res2 );
 
             cal1 = new calculate( Divide );
@@ -65,14 +64,21 @@ namespace Chapter04_02
             tmpOperation -= new calculate( Minus );
             tmpOperation( 7, 3 );
 
+            tmpOperation += new calculate( Multiply );
+            tmpOperation += new calculate( Divide );
+
+            tmpOperation( 3, 2 );
+
+            Console.WriteLine( "Remove" );
+            tmpOperation -= new calculate( Minus );
+            tmpOperation( 7, 3 );
 
             tmpOperation += delegate( int a, int b )
             {
                 Console.WriteLine( "{0}%{1}={2}", a, b, a % b );
                 return (a % b);
             };
-            
-            tmpOperation(4,3);
+            tmpOperation( 4, 3 );
 
             Program tmpPrg = new Program();
 
@@ -80,16 +86,88 @@ namespace Chapter04_02
             tmpPrg.DelegateOper += new calculate( Multiply );
             tmpPrg.DelegateOper( 2, 3 );
 
-
             tmpPrg.EventOper = new calculate( Minus );
-            tmpPrg.EventOper = new calculate( Divide );
+            tmpPrg.EventOper += new calculate( Divide );
             tmpPrg.EventOper( 4, 5 );
 
+            List<string> tmpStrs = new List<string>();
 
+            tmpStrs.Add( "flower" );
+            tmpStrs.Add( "leaf" );
+            tmpStrs.Add( "tree" );
+
+            tmpStrs.Sort( delegate( string a, string b )
+            {
+                return b.CompareTo( a );
+            } );
+            foreach ( string iter in tmpStrs )
+            {
+                Console.WriteLine( iter );
+            }
+            List<int> tmpInts = new List<int>();
+            tmpInts.Add( 51 );
+            tmpInts.Add( 15 );
+            tmpInts.Add( 27 );
+            tmpInts.Add( 41 );
+            tmpInts.Add( 32 );
+
+            tmpInts.Sort( delegate( int a, int b )
+            {
+                return (b.CompareTo( a ));
+            } );
+            Console.WriteLine( "DES" );
+            foreach ( int iter in tmpInts )
+            {
+                Console.WriteLine( iter );
+            }
+            Console.WriteLine( "ASC" );
+            tmpInts.Sort( delegate( int a, int b )
+            {
+                return (a.CompareTo( b ));
+            } );
+            foreach ( int iter in tmpInts )
+            {
+                Console.WriteLine( iter );
+            }
+            tmpInts.Sort( ( int a, int b ) => b.CompareTo( a ) );
+
+            Console.WriteLine( "DES" );
+            foreach ( int iter in tmpInts )
+            {
+                Console.WriteLine( iter );
+            }
+            tmpInts.Sort( ( a, b ) => a.CompareTo( b ) );
+
+            Console.WriteLine( "ASC" );
+            foreach ( int iter in tmpInts )
+            {
+                Console.WriteLine( iter );
+            }
+            calculate AddFunc = ( a, b ) => a + b;
+            calculate MinusFunc = ( a, b ) => a - b;
+
+            Console.WriteLine( "{0}+{1}={2}", 5, 3, AddFunc( 5, 3 ) );
+            Console.WriteLine( "{0}-{1}={2}", 5, 3, MinusFunc( 5, 3 ) );
+
+            Func<int, int, int> MulFunc = ( a, b ) => a * b;
+            Func<string, string, string> ConCat = ( a, b ) => a + b;
+
+            Console.WriteLine( "{0}+{1}={2}", 5, 3, MulFunc( 5, 3 ) );
+            Console.WriteLine( "{0}-{1}={2}", 5, 3, ConCat( "5", "3" ) );
+
+            int resInt = 0;
+            String resStr = "0";
+            Action<int, int> CallAdd = ( a, b ) => resInt = a + b;
+            Action<string, string> CallStr = ( a, b ) => resStr = a + b;
+            CallAdd( 7, 8 );
+            CallStr( "7", "8" );
+
+            Console.WriteLine( "{0}+{1}={2}", 7, 8, resInt );
+            Console.WriteLine( "{0}-{1}={2}", "7", "8", resStr );
             Console.ReadKey();
 
-           
-   
+
+
         }
     }
 }
